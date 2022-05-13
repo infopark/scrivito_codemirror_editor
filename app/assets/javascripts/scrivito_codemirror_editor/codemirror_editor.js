d527d2ab863ff871a3f786a07b6e76c7ca6fc5b0
Alexander Pepper <alexander.pepper@justrelate.com>
2022-05-13 09:31:33 +0200

    Converted "codemirror_editor.js" to plain es5 javascript.

--- app/assets/javascripts/scrivito_codemirror_editor/codemirror_editor.js
+++ app/assets/javascripts/scrivito_codemirror_editor/codemirror_editor.js
@@ -1,21 +1,29 @@
-@scrivito_codemirror_editor =
-  can_edit: (element) -> $(element).is '[data-scrivito-field-type=html]'
-  activate: (element) ->
-    cmsField = $(element)
-    value = cmsField.scrivito 'content'
-    cmsField.empty()
+(function() {
+  var scrivito_codemirror_editor = {
+    can_edit: function(element) {
+      return $(element).is('[data-scrivito-field-type=html]');
+    },
+    activate: function(element) {
+      var cmsField = $(element);
+      var value = cmsField.scrivito('content');
+      cmsField.empty();
+      var options = $.extend({}, scrivito_codemirror_editor.default_options, scrivito_codemirror_editor.options, {
+        value: value
+      });
+      var editor = CodeMirror(element, options);
+      editor.on('change', function(instance) {
+        cmsField.scrivito('save', instance.getValue());
+      });
+    },
+    default_options: {
+      lineNumbers: true,
+      lineWrapping: true
+    },
+    options: {}
+  };
 
-    options = $.extend {},
-      scrivito_codemirror_editor.default_options,
-      scrivito_codemirror_editor.options,
-      value: value
+  scrivito.on('load', function() {
+    scrivito.define_editor('codemirror', scrivito_codemirror_editor);
+  });
 
-    editor = CodeMirror element, options
-
-    editor.on 'change', (instance) -> cmsField.scrivito 'save', instance.getValue()
-  default_options:
-    lineNumbers: true
-    lineWrapping: true
-  options: {}
-
-scrivito.on 'load', -> scrivito.define_editor 'codemirror', scrivito_codemirror_editor
+}).call(this);
